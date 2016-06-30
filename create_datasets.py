d514d98e3e5408ba65007a85886a21ed79695d73
Daniel Marques <danielsmarques7@gmail.com>
2016-06-30 17:36:53 -0300

    Added diabetes dataset.

--- create_datasets.py
+++ create_datasets.py
@@ -4,18 +4,20 @@ from costcla.datasets.base import load_creditscoring2
 from costcla.datasets.base import load_creditgerman
 from costcla.datasets.base import load_kdd98
 from costcla.datasets.base import load_skin
+from costcla.datasets.base import load_diabetes
 from costcla.utils.dataset_folds import create_all_folds
 from costcla.create_cplusplus import create_c_files
 
 num_folds = 10
 train_ratio = 0.8
-dataset_name = "skin"
+dataset_name = "Diabetes_NO_lt30"
 #data = load_creditscoring1()
 #data = load_creditscoring2(nominal_attributes=True)
 #data = load_creditgerman(nominal_attributes=True)
 #data = load_bankmarketing(nominal_attributes=True)
 #data = load_kdd98(as_benefit=True)
-data = load_skin()
+#data = load_skin()
+data = load_diabetes(dataset_name)
 
 ds = create_all_folds(data, num_folds, train_ratio, 0)
-create_c_files(ds, dataset_name, "C:\\Users\\Daniel\\Documents\\DataSets\\Python\\skin\\Folds_10\\")
+create_c_files(ds, dataset_name, "C:\\Users\\Daniel\\Documents\\DataSets\\Python\\diabetes\\NO_lt30\\Folds_10\\")
